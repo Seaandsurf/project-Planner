1f8cef9888c9623f573db60cb6f5cadda4bbd092
Seaandsurf <mathias.hardat@gmail.com>
2024-01-09 16:40:56 +0100

    integration JavaScripts + finition CSS

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# project-Planner
+# Project Planner
+
+-   Create a public repository: `project-planner`
+-   Duration: 3 days
+-   Deadline: 11/01/2024 - 09:00
+-   Submission form: TBD
+
+## Learning objectives
+
+-   Dynamic DOM manipulation
+-   Handling of dates
+-   UX/UI
+-   Use map and filter function
+-   Use a library to manipulate dates [Date fns](https://date-fns.org/)
+
+## The mission
+
+Wouldn't it be nice to have a tool to manage our projects? Something that would allow us to track the progression of each requirements of our apps.
+
+This is the list of features we would like to see:
+
+-   3 columns layout ( one column for Todo, one column for Doing , one column for Done ) If the column is empty, there should be a message saying it's empty
+     *bonus*: Add a secondary view where the layout change into a list. Create a button to toggle between the 2 view. The list view should be sortable ( by urgency or by title ) and filterable ( by status )
+-   Allow user to create tasks. Use a **modal element** with a form inside.
+      Tasks are composed of:
+      - a unique ID ( generated on create, not editable by user)
+      - a date of creation ( generated on create, not editable by user)
+      - a title (required, should be a string with minimum 3 characters and a maximum of 256 characters)
+      - a description (optional, should be a string with minimum 5 character and a maximum of 1024 characters)
+      - a completion status of `to do`, `doing` or `done`) (required, default to `to do`)
+      - a due date (required, default to '2 weeks from the time of creation', should be a valid date)
+        
+-   **Validate** the data coming from the user, if the user didn't indicate a correct information, display an **helpful** error message below the input. ( ex: The date should be of format dd-mm-yyyy, This field is required)
+-   Allow user to update the tasks. (Re-use the modal element and fill the inputs with the task's data, updated data should also be validated)
+-   Allow user to delete the tasks. 
+-   Display the tasks, with their remaining time. For instance, if today is the 15th of March and the task is due for the 20th of March, display `in 5 days`, if it's less then a day it should display in hours, and in minutes if it's less then an hour.
+-   Allow users to sort their tasks by remainining time, with the most urgent first, or by title.
+-   Save tasks in LocalStorage, so that they persist even when the page is refreshed.
+
+
+
+There is a lot of project management tools online. You can get some inspirations from popular tools such as Notion, Trello, Asana or [GitHub Projects](https://docs.github.com/en/issues/trying-out-the-new-projects-experience/about-projects) for instance.
---
+++ scripts/script.js
@@ -0,0 +1,192 @@
+const taskInput = document.getElementById('taskInput');
+const taskDescriptionInput = document.getElementById('taskDescriptionInput');
+const dueDateInput = document.getElementById('dueDateInput');
+const statusFilter = document.getElementById('statusFilter');
+const sortFilter = document.getElementById('sortFilter');
+const taskList = document.getElementById('taskList');
+const deleteCompletedButton = document.getElementById('deleteCompleted');
+
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+updateUI();
+
+document.getElementById('submitTask').addEventListener('click', handleAddTask);
+taskInput.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        handleAddTask();
+    }
+});
+deleteCompletedButton.addEventListener('click', deleteCompletedTasks);
+
+taskList.addEventListener('change', function (event) {
+    if (event.target.type === 'checkbox') {
+        const index = event.target.dataset.index;
+        markAsCompleted(index);
+    }
+});
+
+statusFilter.addEventListener('change', updateUI);
+sortFilter.addEventListener('change', updateUI);
+
+function handleAddTask() {
+    const taskText = taskInput.value.trim();
+    const taskDescription = taskDescriptionInput.value.trim();
+    const dueDate = dueDateInput.value;
+    const status = statusFilter.value;
+    addTask(taskText, taskDescription, dueDate, status);
+    taskInput.value = '';
+    taskDescriptionInput.value = '';
+    dueDateInput.value = '';
+}
+
+function addTask(taskText, taskDescription, dueDate, status) {
+    if (taskText !== '') {
+        const newTask = { text: taskText, description: taskDescription, dueDate: dueDate, status: status, completed: false, createdAt: new Date().toISOString() };
+        tasks.push(newTask);
+        updateLocalStorage();
+        updateUI();
+    }
+}
+
+function markAsCompleted(index) {
+    tasks[index].completed = !tasks[index].completed;
+    tasks[index].status = tasks[index].completed ? 'done' : 'todo';
+    updateLocalStorage();
+    updateUI();
+}
+
+function deleteCompletedTasks() {
+    tasks = tasks.filter(task => !task.completed);
+    updateLocalStorage();
+    updateUI();
+}
+
+function updateLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function updateUI() {
+
+    const sortBy = sortFilter.value;
+    let filteredTasks = filterTasksByStatus(tasks, statusFilter.value);
+    filteredTasks = sortTasks(filteredTasks, sortBy);
+
+    taskList.innerHTML = '';
+
+    filteredTasks.forEach((task, index) => {
+        const listItem = document.createElement('li');
+        listItem.classList.add(task.completed ? 'completed' : null);
+
+        const remainingTimeText = calculateRemainingTime(task.dueDate);
+        const remainingTime = document.createElement('span');
+        remainingTime.classList =()
+        remainingTime.textContent = `Time left: ${remainingTimeText}`;
+        listItem.appendChild(remainingTime);
+
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.checked = task.completed;
+        checkbox.dataset.index = index;
+
+        const taskText = document.createElement('span');
+        const formattedDueDate = formatDate(task.dueDate);
+        taskText.textContent = `${task.text} - ${task.description} (For the: ${formattedDueDate})`;
+
+        // Définir le sélecteur de statut
+        const statusSelector = document.createElement('select');
+        ['todo', 'doing', 'done'].forEach(status => {
+            const option = document.createElement('option');
+            option.value = status;
+            option.textContent = status.charAt(0).toUpperCase() + status.slice(1);
+            option.selected = task.status === status;
+            statusSelector.appendChild(option);
+        });
+        statusSelector.addEventListener('change', () => updateTaskStatus(index, statusSelector.value));
+
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.addEventListener('click', () => deleteTask(index));
+
+
+        const controlsDiv = document.createElement('div');
+        controlsDiv.classList.add('task-controls');
+        controlsDiv.appendChild(statusSelector);
+        controlsDiv.appendChild(deleteButton);
+
+        listItem.appendChild(checkbox);
+        listItem.appendChild(taskText);
+        listItem.appendChild(controlsDiv);
+        taskList.appendChild(listItem);
+    });
+}
+
+
+
+function filterTasksByStatus(tasks, status) {
+    if (status === 'all') {
+        return tasks;
+    } else {
+        return tasks.filter(task => task.status === status);
+    }
+}
+
+
+function formatDate(dateString) {
+    if (!dateString) return 'No Due Date';
+    const options = { year: 'numeric', month: 'long', day: 'numeric', weekday: 'long' };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+}
+
+function deleteTask(index) {
+    tasks.splice(index, 1);
+    updateLocalStorage();
+    updateUI();
+}
+function updateTaskStatus(index, newStatus) {
+    tasks[index].status = newStatus;
+    updateLocalStorage();
+    updateUI();
+}
+function calculateRemainingTime(dueDate) {
+    const now = new Date();
+    const due = new Date(dueDate);
+    const diffInMs = due - now;
+
+    if (diffInMs <= 0) {
+        return 'Past due';
+    }
+
+    const minutes = Math.floor(diffInMs / (1000 * 60));
+    const hours = Math.floor(diffInMs / (1000 * 60 * 60));
+    const days = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
+
+    if (days > 0) {
+        return `in ${days} day(s)`;
+    } else if (hours > 0) {
+        return `in ${hours} hour(s)`;
+    } else {
+        return `in ${minutes} minute(s)`;
+    }
+}
+
+function sortTasks(tasks, sortBy) {
+    if (sortBy === 'time') {
+        return tasks.sort((a, b) => new Date(a.dueDate) - new Date(b.dueDate));
+    } else if (sortBy === 'title') {
+        return tasks.sort((a, b) => a.text.localeCompare(b.text));
+    }
+    return tasks;
+}
+taskInput.addEventListener("change", function () {
+    const inputLength = taskInput.value.length;
+    if (inputLength < 2 || inputLength > 256) {
+        alert("The task name should be between 2 and 256 characters.");
+    }
+});
+
+taskDescriptionInput.addEventListener("change", function () {
+    const descriptionLength = taskDescriptionInput.value.length;
+    if (descriptionLength < 5 || descriptionLength > 1024) {
+        alert("The task description should be between 5 and 1024 characters.");
+    }
+});
